0c93949e0b96edce551b87622063536f09e42fae
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2022-04-28 16:09:19 +0500

    frontend-without-css

---
+++ script.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementsByClassName("canvas");
+var color = document.getElementsByClassName("color");
+var size = document.getElementsByClassName("size");
+var status = document.getElementsByClassName("button");
+
+function drawGrid(){
+
+    
+    
+}
