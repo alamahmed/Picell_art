bcfc593d5488666fa6b368fc3a2d7d799dd2aa1a
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2022-05-02 01:23:07 +0500

    Add back button

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
+//Getting Buttons and canvas from html
 let color = document.getElementById("Color");
-
 let button = document.getElementById("button");
 let canvas = document.getElementById("canvas");
-let hello = document.getElementById("draw_board").style.display = "none";
-
-
 let ctx = canvas.getContext("2d");
+let draw_board = document.getElementById("draw_board");
+let image = document.getElementById("image");
+let Back_Button = document.getElementById("back");
 
+//Getting Offset of the canvas
+let BB = canvas.getBoundingClientRect();
+let offsetX = BB.left;
+let offsetY = BB.top + window.scrollY;
+let paint = false;
 
+//Variables for canvas properties
 let no_of_Boxes = 50;
 canvas.width = innerWidth / 2;
 let box_Size = canvas.width / no_of_Boxes;
 canvas.height = canvas.width;
 
+//Getting Main pages canvas
+let firstCanvas = document.getElementById("firstCanvas");
 
-let BB = canvas.getBoundingClientRect();
-let offsetX = BB.left;
-let offsetY = BB.top + window.scrollY;
+//drawing canvas Visibility hidden and main page visiblility to visible
+draw_board.style.visibility = "hidden";
+image.style.visibility = "visible";
 
-let paint = false;
+//Function to get into the drawing canvas
+function Display(event) {
 
-function startpos(e){
-    paint = true;
-    Draw(e);
+    image.style.visibility = "hidden";
+    draw_board.style.visibility = "visible";
+
 }
 
+//Function for back button
+function back(event) {
+
+    image.style.visibility = "visible";
+    draw_board.style.visibility = "hidden";
+
+}
+
+//Function to start paint
+function startpos(e){
+    paint = true;
+}
+
+//Function to stop paint
 function endpos(){
     paint = false;
 }
 
-    
+//Main function for paint
 function hoverEffect(event){
     if(!paint) return;
     
@@ -44,17 +67,14 @@ function hoverEffect(event){
     
 }
 
+//Converting Mouse Position into index of grid
 function convertMousePositionToCellIndex(mouseX, mouseY, box_Size){
     return {row:Math.trunc(mouseX / box_Size), 
-            col:Math.trunc(mouseY / box_Size)};
+    col:Math.trunc(mouseY / box_Size)};
 }
 
-canvas.addEventListener("mousedown", startpos);
-canvas.addEventListener("mouseup", endpos);
-canvas.addEventListener("mousemove", hoverEffect);
 
-
-
+//TO Draw Grid / Canvas
 function drawBoard(event){
     for (var x = 0; x <= canvas.width; x += box_Size) {
         ctx.moveTo(x, 0);
@@ -70,58 +90,25 @@ function drawBoard(event){
     ctx.strokeStyle = "lightgray";
     ctx.stroke();
 }
+
+//To clean the canvas
 function clean(event){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBoard();
 }
 
+//To take ScreenShot at the Current Position of the canvas
 function screenShot(event){
     let image = canvas.toDataURL('image/jpeg');
 }
 
+//Eevnt Listneres
 button.addEventListener('click', clean, screenShot);
-
-
+Back_Button.addEventListener("click", back);
+firstCanvas.addEventListener("click", Display);
+canvas.addEventListener("mousedown", startpos);
+canvas.addEventListener("mouseup", endpos);
+canvas.addEventListener("mousemove", hoverEffect);
 window.addEventListener('load', drawBoard);
-
-//Functions For Drawing
-// function startpos(e){
-//     paint = true;
-//     Draw(e);
-// }
-
-// function endpos(){
-//     paint = false;
-// }
-
-
-// function Draw(e){
-//     if(!paint) return;
-//     ctx.lineWidth = size.value;
-//     ctx.strokeStyle = color.value;
-//     ctx.lineCap = "round";
-
-    
-//     ctx.beginPath();
-    
-//     ctx.lineTo(e.clientX - offsetX, e.clientY - offsetY);
-//     ctx.stroke()
-    
-
-//     ctx.moveTo(e.clientX - offsetX, e.clientY - offsetY);
-//     ctx.closePath();
-// }
-
-//Event Listeners TO DRAW
-
-// canvas.addEventListener("mousedown", startpos);
-// canvas.addEventListener("mouseup", endpos);
-// canvas.addEventListener("mousemove", Draw);
-
-
-/*===============================================*/
-
-
-//test-X-0, y-0__Return_0, 0
 
 
