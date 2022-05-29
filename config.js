9fb76cbbcc5ea001b9670e2a44afd2833cc19991
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2022-05-29 08:38:04 +0500

    Configure for server

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    MY_KEY : 'AIzaSyBd6AkAihAo7RTklxzNX9obP8FDruyJHCY',
+    Domain : 'picell-8b622.firebaseapp.com',
+    SECRET_KEY : '1:949784715102:web:ef1474f843cf30508e79cf',
+    KEY_2 : 'G-QJFR0KCNMZ',
+    URL : "https://picell-8b622-default-rtdb.europe-west1.firebasedatabase.app",
+    senderID : "949784715102",
+    Storage : "picell-8b622.appspot.com"
+}
